c56c2c3b8d3cb60819d333c8b72d6703e90e8dca
alexauf <mrAlexTFB@gmail.com>
2020-09-07 00:05:00 +0200

    repair tabs for python, modified charging of a firefox profile so u can select the one that you want

--- good_mornings.py
+++ good_mornings.py
@@ -1,4 +1,5 @@
 import selenium
+import glob
 from numpy import arange
 from random import sample
 from sys import exit
@@ -33,51 +34,73 @@ def worker():
 
 class YouMotivate:
 
-    def __init__(self):
+	def __init__(self):
 
-        print('Managing Firefox Info')
-        t = Thread(target=worker)
-        t.start()
+		# UNCOMMENT FOR TEMPORAL BARS WHILE LOADING WITH 60 SECONDS TIME {
+		# print('Managing Firefox Info')
+		# t = Thread(target=worker)
+		# t.start()
+		# }
+		opts = Options()
+		# UNCOMMENT FOR adding firefox user info {
+		users = glob.glob(r"C:\Users\*")
+		print("PC USERS:")
+		users = [user.split("\\")[len(user.split("\\"))-1] for user in users]
+		print(users)
+		print("Choose one: ")
+		user = input()
+		if(not user in users):
+			print("That user does not exist")
+			exit()
 
-        binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
-        fp = (r'C:\Users\AlexMS\AppData\Roaming\Mozilla\Firefox\Profiles\hoaiefpi.default-release')
-        opts = Options()
-        # opts.add_argument("--headless")
-        opts.add_argument(r'load-extension=C:\Users\AlexMS\AppData\Roaming\Mozilla\Firefox\Profiles\hoaiefpi.default-release\extensions\uBlock0@raymondhill.net.xpi')
-        opts.profile = fp
-        self.driver = webdriver.Firefox(options=opts,
-                        executable_path='C:\WebDrivers\geckodriver')
+		binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
+
+		profiles = glob.glob('C:\\Users\\'+str(user)+'\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\*')
+		profiles = [profile.split("\\")[len(profile.split("\\"))-1] for profile in profiles]
+		print("choose profile (normally the one with default-release): ")
+		print(profiles)
+		profile = input()
+		if(not profile in profiles):
+			print("That profile does not exist")
+			exit()
+
+		fp = ('C:\\Users\\'+str(user)+'\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\'+str(profile))
+
+		opts.profile = fp
+		# }
+		self.driver = webdriver.Firefox(options=opts,
+		                executable_path='C:\WebDrivers\geckodriver')
 
 
-        print('Firefox Info Loaded succesfully')
-        print('Opening Youtube...')
+		print('Firefox Info Loaded succesfully')
+		print('Opening Youtube...')
 
-        self.driver.get("https://www.youtube.com/playlist?list=FLHcrPEhUkZW37RI7c5FQvtw")
-        sleep(4)
+		self.driver.get("https://www.youtube.com/playlist?list=FLHcrPEhUkZW37RI7c5FQvtw")
+		sleep(4)
 
 
-        #get num of videos in the list
-        num = self.driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-playlist-sidebar-renderer/div/ytd-playlist-sidebar-primary-info-renderer/div[1]/yt-formatted-string[1]')
-        num = int(num.text.split(' ')[0])
-        # print('NUM OF VIDEOS:\t' + str(num))
+		#get num of videos in the list
+		num = self.driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-playlist-sidebar-renderer/div/ytd-playlist-sidebar-primary-info-renderer/div[1]/yt-formatted-string[1]')
+		num = int(num.text.split(' ')[0])
+		# print('NUM OF VIDEOS:\t' + str(num))
 
-        vids = sample(list(arange(1,num+1)), 3)
-        # print('CHOOSEN:\t' + str(vids))
+		vids = sample(list(arange(1,num+1)), 3)
+		# print('CHOOSEN:\t' + str(vids))
 
-        #choose those videos and open it in new tabs
-        bar = ChargingBar(' Opening videos', max=len(vids))
-        for i in vids:
+		#choose those videos and open it in new tabs
+		bar = ChargingBar(' Opening videos', max=len(vids))
+		for i in vids:
 
-            vid_ref = self.driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-playlist-video-list-renderer/div[3]/ytd-playlist-video-renderer['+str(i)+']/div[2]/a')
-            ref = vid_ref.get_attribute("href")
-            # print(ref)
+		    vid_ref = self.driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-playlist-video-list-renderer/div[3]/ytd-playlist-video-renderer['+str(i)+']/div[2]/a')
+		    ref = vid_ref.get_attribute("href")
+		    # print(ref)
 
-            self.driver.execute_script("window.open('"+str(ref)+"', 'new_window_"+str(i)+"')")
-            # self.driver.execute_script('browser.tabs.create({url:"'+str(ref)+'"});')
+		    self.driver.execute_script("window.open('"+str(ref)+"', 'new_window_"+str(i)+"')")
+		    # self.driver.execute_script('browser.tabs.create({url:"'+str(ref)+'"});')
 
-            bar.next()
+		    bar.next()
 
-        bar.finish()
+		bar.finish()
 
 
 ym = YouMotivate()
